cf5523469fabdb95516c8d2f920834ebaad9fd05
Aidan Lovelace <aidan@aidanlovelace.com>
2019-06-16 23:37:50 -0500

    Updated the example config

--- config.example.js
+++ config.example.js
@@ -1,5 +1,10 @@
+const port = 8080;
+const baseURL = `https://example.com`;
+
 export default {
   JWTsecret: '',
+  baseURL: baseURL,
+  port: port,
   oauth2Credentials: {
     client_id: "",
     project_id: "",
@@ -9,7 +14,8 @@ export default {
     client_secret: "",
     redirect_uris: [],
     scopes: [
-      'https://www.googleapis.com/auth/youtube.readonly'
+      'https://www.googleapis.com/auth/youtube.readonly',
+      'https://www.googleapis.com/auth/youtube.force-ssl'
     ]
   }
 };
